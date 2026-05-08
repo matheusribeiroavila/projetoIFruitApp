7ee2e6d848f694321378ee21a8c668079a704a41
Matheus Ribeiro Avila <ribeiroavilamatheus@gmail.com>
2026-05-08 19:47:05 -0300

    Primeiro Commit

---
+++ src/test/java/br/com/avila/ifruitapp/IfruitappApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.avila.ifruitapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfruitappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
